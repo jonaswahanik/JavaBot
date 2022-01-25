9d3fcbadbbb1dd577aca75be9476ef48364728b0
jonaswahanik <91492982+jonaswahanik@users.noreply.github.com>
2022-01-24 21:48:02 -0800

    finished calorie calculator

--- src/org/vashonsd/Bmr.java
+++ src/org/vashonsd/Bmr.java
@@ -16,41 +16,37 @@ public class Bmr {
 
     }
 
-    public void getBMR() {
+    public double getBMR() {
         double usersBMR1;
         if (gender.equals("male")) {
             usersBMR1 = 66 + (6.3 * weight) + (12.9 * height) - (6.8 * age);
-            //String usersBMR = Double.toString(usersBMR1);
-            System.out.println(usersBMR1);
-
-
         } else {
             usersBMR1 = 665 + (4.3 * weight) + (4.7 * height) - (4.7 * age);
-            //String usersBMR = Double.toString(usersBMR1);
-            System.out.println("wYour BRM is: " + usersBMR1);
-
         }
-        //return usersBMR1;
+        return usersBMR1;
 
 
     }
 
-    public int getCalories() {
-        
+    public void getCalories() {
+        double userBmr = getBMR();
+
         if (exercise == 1) {
+            System.out.println("You should be eating " + (int)(userBmr * 1.2) + " calories per day");
 
         } else if (this.exercise == 2) {
+            System.out.println("You should be eating " + (int)(userBmr * 1.375) + " calories per day");
 
         } else if (this.exercise == 3) {
+            System.out.println("You should be eating " + (int)(userBmr * 1.55) + " calories per day");
 
         } else if (this.exercise == 4) {
+            System.out.println("You should be eating " + (int)(userBmr * 1.725) + " calories per day");
 
         } else {
+            System.out.println("You should be eating " + (int)(userBmr * 1.9) + " calories per day");
 
         }
-
-
     }
 }
-
 
